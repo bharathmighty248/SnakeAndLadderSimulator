902b39a99dfc316a4281010bb6d05452ae1e8059
bharathmighty248 <bharathmighty248@gmail.com>
2021-07-21 17:48:35 +0530

    [Bharath] Add: Option Check Whether Noplay or Snake or Ladder Implemented

--- SnakeAndLadderSimulator/SnakeAndLadder.cs
+++ SnakeAndLadderSimulator/SnakeAndLadder.cs
@@ -12,7 +12,22 @@ namespace SnakeAndLadderSimulator
         {
             Random random = new Random();
             int die = random.Next(1, 7);
-            position = position + die;
+            int option = random.Next(0, 3);
+            switch (option)
+            {
+                case 1:
+                    position += die;
+                    break;
+                case 2:
+                    position -= die;
+                    if (position < 0)
+                        position = 0;
+                    break;
+                default:
+                    position = position;
+                    break;
+
+            }
             Console.WriteLine("Position of Player : " + position);
         }
     }
